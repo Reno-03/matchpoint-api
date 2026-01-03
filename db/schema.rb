1c3cb8d1d8f3d8b98a82e9452db731d7d7552094
Reno <loreenwilmeryboa03@gmail.com>
2026-01-03 14:20:42 +0800

    add Swipe model, who is swiper and who was swiped with like or dislike action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_03_042713) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_03_061511) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -52,6 +52,17 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_03_042713) do
     t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
+  create_table "swipes", force: :cascade do |t|
+    t.string "action"
+    t.datetime "created_at", null: false
+    t.bigint "swiped_id", null: false
+    t.bigint "swiper_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["swiped_id"], name: "index_swipes_on_swiped_id"
+    t.index ["swiper_id", "swiped_id"], name: "index_swipes_on_swiper_id_and_swiped_id", unique: true
+    t.index ["swiper_id"], name: "index_swipes_on_swiper_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.text "bio"
     t.date "birthdate"
@@ -75,4 +86,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_03_042713) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "photos", "users"
+  add_foreign_key "swipes", "users", column: "swiped_id"
+  add_foreign_key "swipes", "users", column: "swiper_id"
 end
